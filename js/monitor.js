f074fa1bf6aa83404865064a709ba3420ee4e40b
sato.shuji <sato.shuji@gmail.com>
2026-02-06 10:53:35 +0900

    Implement 0.1s timer resolution and freeze-on-stop behavior (debug_103)

--- js/monitor.js
+++ js/monitor.js
@@ -331,7 +331,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_102)");
+console.log("Monitor Module Loaded (debug_103)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
