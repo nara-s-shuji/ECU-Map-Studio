fae2cf5d45620296e74eb9340ce5e14856ed1631
sato.shuji <sato.shuji@gmail.com>
2026-02-06 18:58:26 +0900

    Fix file list bottom whitespace and editor header overlap (debug_137)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_136)");
+console.log("Monitor Module Loaded (debug_137)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
