ba9f5c3f6e9eb3c904193b561b913f01ac37a65c
sato.shuji <sato.shuji@gmail.com>
2026-02-06 16:07:22 +0900

    Fix footer spacing with spacer div instead of padding (debug_112)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_111)");
+console.log("Monitor Module Loaded (debug_112)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
