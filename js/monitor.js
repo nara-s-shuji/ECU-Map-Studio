ae5de1e6b89e0bd43d910dcdfeeae3127f09676b
sato.shuji <sato.shuji@gmail.com>
2026-02-06 20:07:43 +0900

    Switch file header to fixed and fix edit layout (debug_142)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_141)");
+console.log("Monitor Module Loaded (debug_142)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
