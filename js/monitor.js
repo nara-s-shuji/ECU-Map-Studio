3066499f4592486e62548d9e6932b6ccda355b7d
sato.shuji <sato.shuji@gmail.com>
2026-02-06 19:03:43 +0900

    Fix file list padding and sticky map header (debug_138)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_137)");
+console.log("Monitor Module Loaded (debug_138)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
