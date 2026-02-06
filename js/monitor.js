e257dd942fec32137d23157ee77a9f528360d667
sato.shuji <sato.shuji@gmail.com>
2026-02-06 19:53:04 +0900

    Update layout for File/Edit view (debug_140)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_139)");
+console.log("Monitor Module Loaded (debug_140)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
