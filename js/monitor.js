356b62623be7c097ba01c6f8783f6fb46dc929ef
sato.shuji <sato.shuji@gmail.com>
2026-02-06 16:22:12 +0900

    Set spacer background to menu color to hide black gap (debug_114)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_113)");
+console.log("Monitor Module Loaded (debug_114)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
