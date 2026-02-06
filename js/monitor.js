e349a1ffff24a51b994176c418771748070bf6d5
sato.shuji <sato.shuji@gmail.com>
2026-02-06 17:50:15 +0900

    Hide monitor scrollbar and increase log height to 3 lines (debug_128)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_127)");
+console.log("Monitor Module Loaded (debug_128)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
