524ec047249f334373ba052b690b52ba54eb3ee7
sato.shuji <sato.shuji@gmail.com>
2026-02-06 15:52:01 +0900

    Align footer buttons to bottom menu with no gap (debug_110)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_109)");
+console.log("Monitor Module Loaded (debug_110)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
