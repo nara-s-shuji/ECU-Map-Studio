b631647355e08f7fcf1b06d44b8dd932948a8a68
sato.shuji <sato.shuji@gmail.com>
2026-02-06 16:00:35 +0900

    Fix black gap by extending footer background behind nav (debug_111)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_110)");
+console.log("Monitor Module Loaded (debug_111)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
