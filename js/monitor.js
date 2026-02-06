95c50c14903c913556b16b3b949e6fac6d022370
sato.shuji <sato.shuji@gmail.com>
2026-02-06 18:42:07 +0900

    Remove margin-bottom from file list header to reduce top whitespace (debug_135)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_134)");
+console.log("Monitor Module Loaded (debug_135)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
