c7f404110ac5f6dc6b55b796b2bbc407fd7288b3
sato.shuji <sato.shuji@gmail.com>
2026-02-06 15:39:25 +0900

    Fix footer visibility bug and adjust log area layout (debug_109)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_108)");
+console.log("Monitor Module Loaded (debug_109)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
