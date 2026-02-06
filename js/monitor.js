ec1f408d65a18575933942b97a866e8781790716
sato.shuji <sato.shuji@gmail.com>
2026-02-06 19:13:24 +0900

    Fix edit view overlap and file view padding (debug_139)

--- js/monitor.js
+++ js/monitor.js
@@ -394,7 +394,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_138)");
+console.log("Monitor Module Loaded (debug_139)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
