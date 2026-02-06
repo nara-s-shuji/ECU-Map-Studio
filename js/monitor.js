a8132e0002ec1743702fc7c5c6f42fb8c580408b
sato.shuji <sato.shuji@gmail.com>
2026-02-06 16:32:40 +0900

    Implement fixed bottom controls to remove layout gaps (debug_115)

--- js/monitor.js
+++ js/monitor.js
@@ -387,7 +387,7 @@ class Monitor {
 // Singleton Instance
 const monitor = new Monitor();
 window.monitor = monitor;
-console.log("Monitor Module Loaded (debug_114)");
+console.log("Monitor Module Loaded (debug_115)");
 
 window.saveDummy = function () {
     alert("Monitor Data Saved (Dummy)");
